94ee10dca3012cf6389603d7851e08785bef323d
IronSublimate <houyuxuan3120487@163.com>
2022-04-06 19:36:05 +0800

    增加环境变量脚本

---
+++ env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+## get environment variable for ros
+
+envir=$(env -i bash -c 'source devel/setup.sh;source /opt/ros/noetic/setup.sh; env')
+
+new_env=""
+
+for line in ${envir}; do
+    new_env=$new_env$line';'
+done
+
+#
+echo $new_env
